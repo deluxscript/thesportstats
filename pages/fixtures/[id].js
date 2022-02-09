92407b92fce7769de5259f25e3cc2621887a6c20
Oladele O <deluxscript@gmail.com>
2022-02-10 00:31:45 +0100

    Develop (#9)
    * edit layout
    
    * add GA
    
    Co-authored-by: Oladele Olorunsola <oladele.olorunsola@bonial.com>

--- pages/fixtures/[id].js
+++ pages/fixtures/[id].js
@@ -1,13 +1,36 @@
 import Layout from '../../components/Layout'
 import SingleTeamStat from '../../components/singleTeamStat'
-import { getFixtures, getTeamStats, head2head, teamStanding } from '../../lib/api'
+import { getFixtures, getResults, getTeamStats, head2head, teamStanding } from '../../lib/api'
 
-const BrandPage = ({ homeStat, awayStat, h2hStats, leagueStanding, homeTeamStanding, awayTeamStanding }) => {
+const BrandPage = ({
+   homeStat,
+   awayStat,
+   h2hStats,
+   last5HomeResults,
+   last10HomeResults,
+   last15HomeResults,
+   last5AwayResults,
+   last10AwayResults,
+   last15AwayResults,
+   leagueStanding,
+   homeTeamStanding,
+   awayTeamStanding
+}) => {
    const sortH2H = h2hStats.response.sort((a, b) => new Date(b.fixture.date) - new Date(a.fixture.date))
    return (
       <Layout>
          <div>
-            <SingleTeamStat home = { homeStat } away = { awayStat } h2h = { sortH2H } />
+            <SingleTeamStat
+               home = { homeStat }
+               away = { awayStat }
+               h2h = { sortH2H }
+               last5HomeResults = { last5HomeResults }
+               last10HomeResults = { last10HomeResults }
+               last15HomeResults = { last15HomeResults }
+               last5AwayResults = { last5AwayResults }
+               last10AwayResults = { last10AwayResults }
+               last15AwayResults = { last15AwayResults }
+            />
          </div>
       </Layout>
    )
@@ -19,13 +42,32 @@ export async function getServerSideProps({ query: { id }}) {
    const homeTeam = getTeamData.response[0].teams.home.id
    const awayTeam = getTeamData.response[0].teams.away.id
 
-   const [getHomeTeamStat, getAwayTeamStat, getHead2Head, getStandings, getHomeTeamStanding, getAwayTeamStanding] = await Promise.all([
+   const [
+      getHomeTeamStat,
+      getAwayTeamStat,
+      getHead2Head,
+      getStandings,
+      getHomeTeamStanding,
+      getAwayTeamStanding,
+      last5HomeResults,
+      last10HomeResults,
+      last15HomeResults,
+      last5AwayResults,
+      last10AwayResults,
+      last15AwayResults,
+   ] = await Promise.all([
       getTeamStats(`team=${homeTeam}&season=2021&league=${getLeague}`),
       getTeamStats(`team=${awayTeam}&season=2021&league=${getLeague}`),
       head2head(`h2h=${homeTeam}-${awayTeam}&status=ft`),
       teamStanding(`league=${getLeague}&season=2021`),
       teamStanding(`league=${getLeague}&season=2021&team=${homeTeam}`),
-      teamStanding(`league=${getLeague}&season=2021&team=${awayTeam}`)
+      teamStanding(`league=${getLeague}&season=2021&team=${awayTeam}`),
+      getResults(`league=${getLeague}&season=2021&team=${homeTeam}&last=5`),
+      getResults(`league=${getLeague}&season=2021&team=${homeTeam}&last=10`),
+      getResults(`league=${getLeague}&season=2021&team=${homeTeam}&last=15`),
+      getResults(`league=${getLeague}&season=2021&team=${awayTeam}&last=5`),
+      getResults(`league=${getLeague}&season=2021&team=${awayTeam}&last=10`),
+      getResults(`league=${getLeague}&season=2021&team=${awayTeam}&last=15`),
    ])
 
 
@@ -36,7 +78,13 @@ export async function getServerSideProps({ query: { id }}) {
          h2hStats: getHead2Head,
          leagueStanding: getStandings,
          homeTeamStanding: getHomeTeamStanding,
-         awayTeamStanding: getAwayTeamStanding
+         awayTeamStanding: getAwayTeamStanding,
+         last5HomeResults: last5HomeResults,
+         last10HomeResults: last10HomeResults,
+         last15HomeResults: last15HomeResults,
+         last5HomeResults: last5AwayResults,
+         last10AwayResults: last10AwayResults,
+         last15AwayResults: last15AwayResults,
       },
    }
  }
