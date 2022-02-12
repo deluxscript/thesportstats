01cc999924a85d26e384319e6c3b4542412acecc
Oladele Olorunsola <oladele.olorunsola@bonial.com>
2022-02-12 12:10:03 +0100

    restructure url pattern

--- components/Fixture.js
+++ components/Fixture.js
@@ -11,7 +11,7 @@ export default function Fixture({ id, teams, time, status}) {
          <div className="w-1/12 text-xs">{ formatTime(time)}</div>
          <div className="md:w-1/5 text-xs">{ status.short }</div>
          <div>
-            <Link href={`/fixtures/${id}`}>
+            <Link href={`/fixtures/${(teams.home.name).toLowerCase().replace(/\s+/g, '-')}-vs-${(teams.away.name).toLowerCase().replace(/\s+/g, '-')}?id=${id}`}>
                <a className="rounded-md bg-statsBgColor text-xs p-2">
                   stats
                </a>
